d888b2f8c9038d0a5013c913f0079ea98e98371b
elliedori <elliedori@gmail.com>
2024-01-10 17:23:55 -0800

    Add tweaks to play nice with CodeSpaces environment

--- install.sh
+++ install.sh
@@ -3,13 +3,11 @@
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd)"
 echo $DIR
 
-rm "$HOME/.zshrc"
-
 ln -s "$DIR/.bash_profile" "$HOME/.bash_profile"
 # ln -s "$DIR/.bashrc" "$HOME/.bashrc"
 ln -s "$DIR/.gitconfig" "$HOME/.gitconfig"
 # ln -s "$DIR/.inputrc" "$HOME/.inputrc"
 ln -s "$DIR/.zshrc" "$HOME/.zshrc"
 
-# echo "Installing zsh."
-# KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing zsh."
+KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
